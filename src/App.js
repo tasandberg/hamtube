066ee255136ac86a5328a3ff892d2e035c7334fb
Tim Sandberg <tasandberg@gmail.com>
2020-04-05 21:25:28 -0700

    node-sass, react-router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Router from "./Router"
+import "./styles/main.scss"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <Router />
 }
-
-export default App;
