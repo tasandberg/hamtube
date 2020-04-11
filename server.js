fa5e933bd105248123f0ee0b9acd76db0d1ed649
Tim Sandberg <tasandberg@gmail.com>
2020-04-10 23:00:47 -0700

    Add POST /new-chat route

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const app = express()
 // const bodyParser = require("body-parser")
 const path = require("path")
 const port = 4000
+const newChatRoute = require("./util/new-chat")
 
 app.use(express.static(path.join(__dirname, "build")))
 const http = require("http")
 const server = http.createServer(app)
 require("./util/socket.js")(server)
+
 if (process.env.NODE_ENV === "production") {
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "public", "index.html"))
@@ -17,6 +19,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "public", "index.html"))
   })
 }
+app.post("/new-chat", newChatRoute)
 
 server.listen(process.env.PORT || port, () =>
   console.log(`Server is running on port ${port}`)
