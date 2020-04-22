481731ffb7e754310845235b59b9c32b38740c4e
Tim Sandberg <tasandberg@gmail.com>
2020-04-21 23:13:10 -0700

    wip towards socket-driven video control

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,77 +1,66 @@
-import React from "react"
-import YouTube from "react-youtube"
-import { extractVideoId } from "../util/youtube-data"
-import classNames from "classnames"
+import React, { Fragment } from "react";
+import YouTube from "react-youtube";
+import { extractVideoId } from "../util/youtube-data";
+import classNames from "classnames";
 
 class VideoPlayer extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       isPlaying: false,
       progress: 0,
-    }
+    };
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    const oldVideoState = prevProps.videoState
-    const videoState = this.props.videoState
-
-    if (this.props.videoState !== oldVideoState && !this.props.isLocalUser) {
-      console.log("video state change", videoState)
-      if (videoState === YouTube.PlayerState.PLAYING) {
-        this.player.playVideo()
-      }
-    }
+    const oldVideoState = prevProps.videoState;
+    const videoState = this.props.videoState;
   }
 
   setPlaying() {
     setTimeout(() => {
-      this.setState({ isPlaying: true })
-    }, 2000)
+      this.setState({ isPlaying: true });
+    }, 2000);
   }
 
   updateProgress() {
-    const currentTime = this.player.getCurrentTime()
-    const duration = this.player.getDuration()
-    this.props.setCurrentTime(currentTime)
+    const currentTime = this.player.getCurrentTime();
+    const duration = this.player.getDuration();
+    this.props.setCurrentTime(currentTime);
     this.setState({
       progress: (currentTime / duration) * 100,
-    })
+    });
     setTimeout(() => {
-      this.updateProgress()
-    }, 200)
+      this.updateProgress();
+    }, 200);
   }
 
   stateChange = (e) => {
-    const { data } = e
+    const { data } = e;
 
-    this.updateProgress()
-    if (data === YouTube.PlayerState.PLAYING) this.setPlaying()
+    this.updateProgress();
+    if (data === YouTube.PlayerState.PLAYING) this.setPlaying();
 
     if (this.props.isLocalUser) {
-      this.props.broadcast(data)
+      this.props.broadcast(data);
     }
-  }
+  };
 
   onReady = (e) => {
-    console.log("video player ready")
-    this.player = e.target
-    if (this.props.isLocalUser) {
-      setTimeout(() => e.target.playVideo(), 2000)
-    }
-  }
+    console.log("video player ready");
+    this.player = e.target;
+  };
 
   renderVideoPlayer(resource, isLocalUser) {
-    const { videoData } = resource
+    const { videoData } = resource;
 
     return (
-      <div className="video-container">
+      <Fragment>
         <div
           className={classNames("video-overlay", {
             fade: this.state.isPlaying,
-          })}
-        >
+          })}>
           <div className="loading-text has-text-white">
             <center>
               <h1 className="title is-1 has-text-white">{videoData.title}</h1>
@@ -100,18 +89,22 @@ class VideoPlayer extends React.Component {
           }}
           onReady={this.onReady}
         />
-      </div>
-    )
+      </Fragment>
+    );
   }
 
   render() {
-    const { resource, isLocalUser } = this.props
-    return resource ? (
-      this.renderVideoPlayer(resource, isLocalUser)
-    ) : (
-      <span>womp womp</span>
-    )
+    const { currentSong, isLocalUser } = this.props;
+    return (
+      <div className="video-container">
+        {currentSong ? (
+          this.renderVideoPlayer(currentSong, isLocalUser)
+        ) : (
+          <span>TODO: Empty video state</span>
+        )}
+      </div>
+    );
   }
 }
 
-export default VideoPlayer
+export default VideoPlayer;
--- util/socket.js
+++ util/socket.js
@@ -1,7 +1,7 @@
 // const youtubedl = require("youtube-dl")
 
-const getNumberWithOrdinal = require("./numberHelper")
-const _ = require("lodash")
+const getNumberWithOrdinal = require("./numberHelper");
+const _ = require("lodash");
 const PlayerState = {
   UNSTARTED: -1,
   ENDED: 0,
@@ -9,7 +9,7 @@ const PlayerState = {
   PAUSED: 2,
   BUFFERING: 3,
   CUED: 5,
-}
+};
 
 /**
  * Dictionary of songQueues by RoomID
@@ -18,168 +18,171 @@ const PlayerState = {
  *    { singerId: socketId, videoData: videoData}
  *  ]
  */
-const songQueues = {}
-const nowPlaying = {}
-const videoPosition = {}
+const songQueues = {};
+const nowPlaying = {};
+const videoPosition = {};
 
 function initializeSongQueue(roomId) {
-  if (songQueues[roomId]) return
-  songQueues[roomId] = []
+  if (songQueues[roomId]) return;
+  songQueues[roomId] = [];
 }
 
 function addToSongQueue(roomId, videoData, singerId) {
-  songQueues[roomId].push({ singerId, videoData })
+  songQueues[roomId].push({ singerId, videoData });
 }
 
 module.exports = function (server) {
-  const io = require("socket.io")(server)
-  io.sockets.on("error", (e) => console.log(e))
+  const io = require("socket.io")(server);
+  io.sockets.on("error", (e) => console.log(e));
 
   const onConnection = (socket) => {
-    const roomId = socket.handshake.query.room
+    const roomId = socket.handshake.query.room;
 
-    socket.join(roomId)
-    initializeSongQueue(roomId)
+    socket.join(roomId);
+    initializeSongQueue(roomId);
 
-    console.log("Connection to room %s with ID: %s", roomId, socket.id)
+    console.log("Connection to room %s with ID: %s", roomId, socket.id);
 
     /**
      * Peer Signaling Connections
      */
     const connectedSocketIds = Object.keys(
       io.sockets.adapter.rooms[roomId].sockets
-    ).filter((id) => id !== socket.id)
-    const connectedSockets = _.pick(io.sockets.connected, connectedSocketIds)
+    ).filter((id) => id !== socket.id);
+    const connectedSockets = _.pick(io.sockets.connected, connectedSocketIds);
 
     _.forEach(connectedSockets, function (socket2) {
-      console.log("Advertising peer %s to %s", socket.id, socket2.id)
+      console.log("Advertising peer %s to %s", socket.id, socket2.id);
       socket2.emit("peer", {
         peerId: socket.id,
         initiator: true,
-      })
+      });
 
       socket.emit("peer", {
         peerId: socket2.id,
         initiator: false,
-      })
+      });
 
-      io.to(socket.id).emit("room-data", {
-        upNext: songQueues[roomId][0],
-        nowPlaying: nowPlaying[roomId],
-        position: videoPosition[roomId],
-      })
-    })
+      /**
+       * Can only fire when video data
+       */
+      // io.to(socket.id).emit("room-data", {
+      //   upNext: songQueues[roomId][0],
+      //   nowPlaying: nowPlaying[roomId],
+      //   position: videoPosition[roomId],
+      // });
+    });
 
     /**
      * General Use Room Functions
      */
     const notifyRoom = (message, excludeUser = false) => {
-      let notifier = excludeUser ? socket.to(roomId) : io.to(roomId)
-      notifier.emit("notification", { message })
-    }
+      let notifier = excludeUser ? socket.to(roomId) : io.to(roomId);
+      notifier.emit("notification", { message });
+    };
 
     /**
      * Song Queue Functions
      */
     const addToSongQueue = (videoData) => {
-      songQueues[roomId].push({ singerId: socket.id, videoData })
-    }
+      songQueues[roomId].push({ singerId: socket.id, videoData });
+    };
 
     const setNowPlaying = (videoData) => {
-      console.log("Setting now playing")
+      console.log("Setting now playing");
 
-      nowPlaying[roomId] = videoData
-    }
+      nowPlaying[roomId] = videoData;
+    };
 
     const cycleSong = () => {
-      console.log("Cycling songs")
+      console.log("Cycling songs");
 
-      const currentSong = songQueues[roomId].shift()
-      setNowPlaying(currentSong)
-      const upNext = songQueues[roomId][0]
+      const currentSong = songQueues[roomId].shift();
+      setNowPlaying(currentSong);
+      const upNext = songQueues[roomId][0];
 
       io.to(roomId).emit("new-song", {
         currentSong,
         currentSinger: currentSong.singerId,
         upNext,
-      })
+      });
 
-      notifyRoom(`Now playing: ${currentSong.videoData.title}`)
-    }
+      notifyRoom(`Now playing: ${currentSong.videoData.title}`);
+    };
 
     // Event Handlers
     socket.on("disconnect", function (peerId) {
-      console.log("Disconnecting ", socket.id)
+      console.log("Disconnecting ", socket.id);
       console.log(
         Object.keys(io.sockets.connected).length,
         "clients connected to room " + roomId
-      )
+      );
 
-      io.to(roomId).emit("destroy", socket.id)
-    })
+      io.to(roomId).emit("destroy", socket.id);
+    });
 
     socket.on("disconnect-video", function () {
-      socket.to(roomId).emit("disconnect-video", socket.id)
-    })
+      socket.to(roomId).emit("disconnect-video", socket.id);
+    });
 
     socket.on("video-sync", (data) => {
       switch (data) {
         case PlayerState.PLAYING:
-          console.log("video playing")
-          break
+          console.log("video playing");
+          break;
         case PlayerState.BUFFERING:
-          console.log("video buffering")
-          break
+          console.log("video buffering");
+          break;
         case PlayerState.ENDED:
-          console.log("video ended")
-          break
+          console.log("video ended");
+          break;
         default:
-          console.log(data, "video-sync")
-          break
+          console.log(data, "video-sync");
+          break;
       }
-    })
+    });
 
     socket.on("video-position", (data) => {
-      console.log(data, "video-position")
-      videoPosition[roomId] = data
-    })
+      console.log(data, "video-position");
+      videoPosition[roomId] = data;
+    });
 
     socket.on("signal", function (data) {
-      var socket2 = io.sockets.connected[data.peerId]
+      var socket2 = io.sockets.connected[data.peerId];
       if (!socket2) {
-        return
+        return;
       }
 
       socket2.emit("signal", {
         signal: data.signal,
         peerId: socket.id,
-      })
-    })
+      });
+    });
 
     socket.on("add-song", (data) => {
-      addToSongQueue(data)
-      const queueLength = songQueues[roomId].length
+      addToSongQueue(data);
+      const queueLength = songQueues[roomId].length;
 
-      console.log(`Song added for room ${roomId}. ${queueLength} total.`)
+      console.log(`Song added for room ${roomId}. ${queueLength} total.`);
 
       // Send event to all except user who added song
       notifyRoom(
         `A new song was just added to the queue 👻 (${queueLength} total)`,
         true
-      )
+      );
 
       // Send success feedback to user
       io.to(socket.id).emit("song-added-success", {
         message: `Song added. It's ${getNumberWithOrdinal(
           songQueues[roomId].length
         )} in line. 🔥`,
-      })
+      });
 
       if (!nowPlaying[roomId]) {
-        cycleSong()
+        cycleSong();
       }
-    })
-  }
+    });
+  };
 
-  io.on("connection", onConnection)
-}
+  io.on("connection", onConnection);
+};
