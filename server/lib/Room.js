c11f9d1e09ee1b24a537af3d09102312f8e124af
Tim Sandberg <tasandberg@gmail.com>
2020-04-26 10:47:22 -0700

    remove side effect (cycleSongs) from addSongToQueue

--- server/lib/Room.js
+++ server/lib/Room.js
@@ -10,68 +10,66 @@
 
 export default class Room {
   constructor({ id, io }) {
-    this.id = id;
-    this.io = io;
-    this.songQueue = [];
-    this.nowPlaying = null;
-    this.videoPosition = 0;
-    this.users = [];
-    this.awaitingClients = null;
+    this.id = id
+    this.io = io
+    this.songQueue = []
+    this.nowPlaying = null
+    this.videoPosition = 0
+    this.users = []
+    this.awaitingClients = null
   }
 
   addToSongQueue = (videoData, userId, cb) => {
-    songQueue.push({
+    this.songQueue.push({
       singerId: userId,
       videoData,
-    });
+    })
 
-    console.log(
-      `Song added for room ${roomId}. ${this.songQueue.length} total.`
-    );
-
-    cb(this);
-
-    if (!nowPlaying[roomId]) {
-      cycleSong();
+    if (cb) {
+      cb(this.roomData())
     }
-  };
+  }
 
   roomData = () => ({
     currentSong: this.nowPlaying,
-    currentSinger: currentSong ? currentSong.singerId : null,
+    currentSinger: this.nowPlaying ? this.nowPlaying.singerId : null,
     upNext: this.songQueue[0],
     position: this.videoPosition,
-  });
+  })
 
   songIsPlaying = () => {
-    return !!this.nowPlaying;
-  };
-
-  getUpNext = () => {
-    return getSongQueue()[0];
-  };
+    return !!this.nowPlaying
+  }
 
   // Remove and return song at top of queue
   advanceQueue = () => {
-    this.videoPosition = 0;
-    this.nowPlaying = this.songQueue.shift();
-    return this.nowPlaying;
-  };
+    this.videoPosition = 0
+    this.nowPlaying = this.songQueue.shift()
+    return this.nowPlaying
+  }
 
   refreshAwaitingClients = () => {
-    this.awaitingClients = this.users.map((socket) => socket.id);
-  };
+    this.awaitingClients = this.users.map((socket) => socket.id)
+  }
 
-  cycleSong = () => {
-    console.log("Cycling songs");
-    const currentSong = advanceQueue();
+  cycleSong = (cb) => {
+    const currentSong = this.advanceQueue()
 
     if (currentSong) {
-      notifyRoom(`Now playing: ${currentSong.videoData.title}`);
-      refreshAwaitingClients();
+      this.refreshAwaitingClients()
+      cb &&
+        cb({
+          message: `Now playing: ${currentSong.videoData.title}`,
+          roomData: this.roomData(),
+        })
+      return true
     } else {
-      console.log("No more songs");
+      cb &&
+        cb({
+          message: `No songs left in Queue. Add more!`,
+          roomData: this.roomData(),
+        })
+      return false
     }
-    broadcastRoomData();
-  };
+  }
 }
--- src/tests/server/Room.test.js
+++ src/tests/server/Room.test.js
@@ -1,40 +1,69 @@
-import Room from "../../../server/lib/Room";
-import { forEach } from "lodash";
+import Room from "../../../server/lib/Room"
+import { forEach } from "lodash"
 
-const newRoom = new Room({ id: "abc" });
+let newRoom
+beforeEach(() => {
+  newRoom = new Room({ id: "abc" })
+})
 
 /**
  * Constructor Tests
  */
-it("has a valid constructor", () => {
-  new Room({ id: "abc" });
-});
+describe("init", () => {
+  it("has a valid constructor", () => {
+    new Room({ id: "abc" })
+  })
 
-it("sets the room ID to the given id", () => {
-  expect(newRoom.id).toEqual("abc");
-});
+  it("sets the room ID to the given id", () => {
+    expect(newRoom.id).toEqual("abc")
+  })
 
-const expectedInitialProperties = {
-  songQueue: [],
-  nowPlaying: null,
-  videoPosition: 0,
-  users: [],
-};
-forEach(expectedInitialProperties, (value, key) => {
-  it(`initializes ${key} to ${value}`, () => {
-    expect(newRoom[key]).toEqual(value);
-  });
-});
+  const expectedInitialProperties = {
+    songQueue: [],
+    nowPlaying: null,
+    videoPosition: 0,
+    users: [],
+  }
+  forEach(expectedInitialProperties, (value, key) => {
+    it(`initializes ${key} to ${value}`, () => {
+      expect(newRoom[key]).toEqual(value)
+    })
+  })
+})
 
-/**
- * Operations tests
- */
+describe("addToSongQueue", () => {
+  const callback = jest.fn()
 
-/**
- * Adding song to queue
- * - Added song to end of queue,
- * - Calls callback with self
- */
+  // Add two songs to queue
+  const addSongs = () => {
+    newRoom.addToSongQueue({ id: "first-song" }, "user-1", callback)
+    newRoom.addToSongQueue({ id: "second-song" }, "user-2", callback)
+    newRoom.addToSongQueue({ id: "third-song" }, "user-3", callback)
+  }
+
+  it("adds song to queue with proper format", () => {
+    addSongs()
+    const lastSong = newRoom.songQueue.slice(-1).pop()
+
+    expect(lastSong).toEqual({
+      singerId: "user-3",
+      videoData: { id: "third-song" },
+    })
+  })
+
+  it("adds songs to queue in FIFO order", () => {
+    addSongs()
+    const queueIds = newRoom.songQueue.map((s) => s.singerId)
+    expect(queueIds).toEqual(["user-1", "user-2", "user-3"])
+  })
+
+  it("passes the room data to the callback", () => {
+    const callback = jest.fn()
+    newRoom.addToSongQueue({ id: "abc" }, "user", callback)
+    expect(callback).toHaveBeenCalledTimes(1)
+    expect(callback).toHaveBeenCalledWith(newRoom.roomData())
+  })
+})
 
 /**
  * Cycling song
@@ -44,6 +73,41 @@ forEach(expectedInitialProperties, (value, key) => {
  *  - If end of list, <song> to null
  * - Calls callback with self
  */
+describe("cycleSong", () => {
+  it("calls advanceQueue", () => {
+    const advanceQueueMock = jest.fn()
+    newRoom.advanceQueue = advanceQueueMock
+    newRoom.cycleSong()
+    expect(advanceQueueMock).toHaveBeenCalledTimes(1)
+  })
+
+  describe("when songs are in queue", () => {
+    it("returns true", () => {
+      newRoom.songQueue = [{ videoData: { id: "abc" }, singerId: "user" }]
+      expect(newRoom.cycleSong()).toEqual(true)
+    })
+
+    it("calls refreshAwaitingClients if there was a song to advance", () => {
+      newRoom.songQueue = [{ videoData: { id: "abc" }, singerId: "user" }]
+      const refreshAwaitingClientsMock = jest.fn()
+      newRoom.refreshAwaitingClients = refreshAwaitingClientsMock
+      newRoom.cycleSong()
+      expect(refreshAwaitingClientsMock).toHaveBeenCalledTimes(1)
+    })
+
+    it("passes success message to callback", () => {
+      newRoom.songQueue = [
+        { videoData: { id: "abc", title: "Hogwash" }, singerId: "user" },
+      ]
+      const callback = jest.fn()
+      newRoom.cycleSong(callback)
+      expect(callback).toHaveBeenCalledWith({
+        message: "Now playing: Hogwash",
+        roomData: newRoom.roomData(),
+      })
+    })
+  })
+})
 
 /**
  * Up Next: returns top song in queue
